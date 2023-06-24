d43912a7d1b35474ffed94fcac1978702d32a131
Erik Simonian <ericsim90210@gmail.com>
2023-06-24 22:56:01 +0500

    Added Problem 199

---
+++ Trees/Problem 199 Binary Tree Right Side View.py
@@ -0,0 +1,68 @@
+"""199. Binary Tree Right Side View (Difficulty: Medium).
+
+Given the root of a binary tree, imagine yourself standing on the right side of it,
+return the values of the nodes you can see ordered from top to bottom.
+
+Example 1:
+Input: root = [1,2,3,null,5,null,4]
+Output: [1,3,4]
+
+Example 2:
+Input: root = [1,null,3]
+Output: [1,3]
+
+Example 3:
+Input: root = []
+Output: []
+
+Constraints:
+    The number of nodes in the tree is in the range [0, 100].
+    -100 <= Node.val <= 100"""
+import collections
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution199:
+    def rightSideView(self, root: TreeNode) -> list[int]:   # Breadth First Search method
+        result = []
+        queue = collections.deque([root])
+
+        while queue:
+            rightmost_element = None
+            queue_length = len(queue)
+
+            for i in range(queue_length):
+                node = queue.popleft()
+                if node:
+                    rightmost_element = node
+                    queue.append(node.left)
+                    queue.append(node.right)
+
+            if rightmost_element:
+                result.append(rightmost_element.val)
+
+        return result
+
+
+tree_root1 = TreeNode(1)
+tree_root1.left = TreeNode(2)
+tree_root1.right = TreeNode(3)
+tree_root1.left.right = TreeNode(5)
+tree_root1.right.right = TreeNode(4)
+
+tree_root2 = TreeNode(1)
+tree_root2.right = TreeNode(3)
+
+tree_root3 = TreeNode()
+
+solution = Solution199()
+print(solution.rightSideView(tree_root1))
+print(solution.rightSideView(tree_root2))
+print(solution.rightSideView(tree_root3))
+
