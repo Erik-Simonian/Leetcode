d6ad3e0d1832e6abb2e64db5805280436fc2836c
Erik Simonian <ericsim90210@gmail.com>
2023-07-15 23:53:22 +0500

    Added Problem 211

---
+++ Trees/Problem 211 Design Add and Search Words Data Structure.py
@@ -0,0 +1,90 @@
+"""211. Design Add and Search Words Data Structure (Difficulty: Medium).
+
+Design a data structure that supports adding new words and finding if a string matches any previously added string.
+
+Implement the WordDictionary class:
+    WordDictionary() Initializes the object.
+    void addWord(word) Adds word to the data structure, it can be matched later.
+    bool search(word) Returns true if there is any string in the data structure that matches word or false otherwise.
+    word may contain dots '.' where dots can be matched with any letter.
+
+Example:
+
+Input
+["WordDictionary","addWord","addWord","addWord","search","search","search","search"]
+[[],["bad"],["dad"],["mad"],["pad"],["bad"],[".ad"],["b.."]]
+Output
+[null,null,null,null,false,true,true,true]
+
+Explanation
+WordDictionary wordDictionary = new WordDictionary();
+wordDictionary.addWord("bad");
+wordDictionary.addWord("dad");
+wordDictionary.addWord("mad");
+wordDictionary.search("pad"); // return False
+wordDictionary.search("bad"); // return True
+wordDictionary.search(".ad"); // return True
+wordDictionary.search("b.."); // return True
+
+Constraints:
+    1 <= word.length <= 25
+    word in addWord consists of lowercase English letters.
+    word in search consist of '.' or lowercase English letters.
+    There will be at most 2 dots in word for search queries.
+    At most 104 calls will be made to addWord and search."""
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        current_node = self.root
+
+        for character in word:
+            if character not in current_node.children:
+                current_node.children[character] = TrieNode()
+            current_node = current_node.children[character]
+
+        current_node.is_word = True
+
+
+    def search(self, word: str) -> bool:
+        def dfs(index, root):
+            current_node = self.root
+
+            for i in range(index, len(word)):
+                char = word[i]
+
+                if char == ".":
+                    for child in current_node.children.values():
+                        if dfs(i + 1, child):
+                            return True
+                    return False
+
+                else:
+                    if char not in current_node.children:
+                        return False
+                    current_node = current_node.children[char]
+
+            return current_node.is_word
+
+        return dfs(0, self.root)
+
+
+
+
+obj = WordDictionary()
+obj.addWord("bad")
+obj.addWord("dad")
+obj.addWord("mad")
+print(obj.search("pad"))
+print(obj.search("bad"))
+print(obj.search(".ad"))
+print(obj.search("b.."))
