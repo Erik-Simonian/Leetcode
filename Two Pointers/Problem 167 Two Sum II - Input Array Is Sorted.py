fb3e7daea926cc7e1f1608f2a8ab2d1bb284f434
Erik Simonian <ericsim90210@gmail.com>
2023-09-12 17:54:13 +0500

    Fixed typos in Problem 167

--- Two Pointers/Problem 167 Two Sum II - Input Array Is Sorted.py
+++ Two Pointers/Problem 167 Two Sum II - Input Array Is Sorted.py
@@ -4,7 +4,8 @@ https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order,
 find two numbers such that they add up to a specific target number.
 Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 < numbers.length.
-Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+Return the indices of the two numbers, index1 and index2,
+added by one as an integer array [index1, index2] of length 2.
 The tests are generated such that there is exactly one solution. You may not use the same element twice.
 Your solution must use only constant extra space.
 
@@ -49,7 +50,7 @@ class Solution167:
         return []
 
 
-soluiton = Solution167()
-print(soluiton.twoSum(numbers=[2, 7, 11, 15], target=9))
-print(soluiton.twoSum(numbers=[2, 3, 4], target=6))
-print(soluiton.twoSum(numbers=[-1, 0], target=-1))
+solution = Solution167()
+print(solution.twoSum(numbers=[2, 7, 11, 15], target=9))
+print(solution.twoSum(numbers=[2, 3, 4], target=6))
+print(solution.twoSum(numbers=[-1, 0], target=-1))
